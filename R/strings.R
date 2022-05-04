786be5c8236bdf13d3c6f34964ec33fd80c08231
Clément Turbelin <clement.turbelin@upmc.fr>
2022-05-04 17:36:32 +0200

    adding file_stamp

--- R/strings.R
+++ R/strings.R
@@ -20,3 +20,25 @@ wrap_string = function(x, before=NULL, after=NULL) {
 ending_slash = function(x) {
   paste0(x, ifelse(grepl("/$", x), "", "/"))
 }
+
+#' Create a time based stamp to be used as suffix in file names
+#' @param time string with type of time string to use, "datetime" will output date and time (with only separator between date and date like 20220402-103402 for 2 May 2022, 10:34:02), default is "datetime"
+#' @param random integer length of random string to add to the time, no random if 0 (default behavior)
+#' @param sep string with separator to use between time and random part
+#' @export
+file_stamp = function(time="datetime", random=0, sep="-") {
+  now = Sys.time()
+  if(time == "timestamp") {
+    stamp = as.character(as.integer(now))
+  }
+  fmt = switch(time,
+    datetime="%Y%m%d-%H%M%S",
+    date="%Y%m%d",
+    time
+  )
+  stamp = format(now, format=fmt)
+  if(random > 0) {
+    stamp = paste(stamp, random_string(random), sep=sep)
+  }
+  stamp
+}
